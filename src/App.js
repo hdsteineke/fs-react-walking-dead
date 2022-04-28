4c3be5ce2c9fb9867d88e932fe03e4f1110a9546
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-04-28 16:16:40 -0700

    zombie fight rendering on page, each character grows and shrinks accordingly

--- src/App.js
+++ src/App.js
@@ -1,21 +1,79 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [zombieSize, setZombieSize] = useState(3);
+  const [humanSize, setHumanSize] = useState(3);
+  const [isOpen, setIsOpen] = useState(false);
+  const [zombieArray, setZombieArray] = useState([
+    'zombie1',
+    'brain',
+    'zombie2',
+    'dumpster',
+    'fire',
+  ]);
+
+  const zombieStyle = {
+    fontSize: `${zombieSize}rem`,
+  };
+
+  const humanStyle = {
+    fontSize: `${humanSize}rem`,
+  };
+
+  function handleAddZombie1() {
+    zombieArray.push('zombie1');
+
+    setZombieArray([...zombieArray, 'zombie1']);
+  }
+
+  function handleAddBrain() {
+    zombieArray.push('brain');
+
+    setZombieArray([...zombieArray, 'brain']);
+  }
+
+  function handleAddZombie2() {
+    zombieArray.push('zombie2');
+
+    setZombieArray([...zombieArray, 'zombie2']);
+  }
+
+  function handleAddDumpster() {
+    zombieArray.push('dumpster');
+
+    setZombieArray([...zombieArray, 'dumpster']);
+  }
+
+  function handleAddFire() {
+    zombieArray.push('fire');
+
+    setZombieArray([...zombieArray, 'fire']);
+  }
+
+  const ATTACK = 1;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="fight">
+        <div className="zombie">
+          <p style={zombieStyle}>🧟</p>
+          <button onClick={() => setZombieSize(zombieSize + ATTACK)}>
+            Zombie slurps braaiiinnss
+          </button>
+          <button onClick={() => setHumanSize(humanSize - ATTACK)}>Zombie shrieks</button>
+        </div>
+
+        <div className="human">
+          <p style={humanStyle}>🙅</p>
+          <button onClick={() => setHumanSize(humanSize + ATTACK)}>Human drinks Gatorade</button>
+          <button onClick={() => setZombieSize(zombieSize - ATTACK)}>
+            Human does the truffle shuffle
+          </button>
+        </div>
+      </section>
+
+      <section className="sign"></section>
     </div>
   );
 }
